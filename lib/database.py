885e16e2b1965543e514ad0790243a88695ae7d3
Tri Angga Dio Simamora <trianggadio@gmail.com>
2021-06-17 00:07:25 +0700

    change to env

--- lib/database.py
+++ lib/database.py
@@ -1,7 +1,10 @@
 import psycopg2
-import config
-
+import os
 
 def db_connect():
-    conn = psycopg2.connect(host=config.host, database=config.database, user=config.user, password=config.password)
+    host = os.environ.get('DB_HOST')
+    database = os.environ.get('DB_DATABASE')
+    user = os.environ.get('DB_USER')
+    password = os.environ.get('DB_PASSWORD')
+    conn = psycopg2.connect(host=host, database=database, user=user, password=password)
     return conn
